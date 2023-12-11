b9dc556f163d72cf5d55e602eea67d0999e2f65f
Sacha <le1829079@crc-lennox.qc.ca>
2023-12-11 17:33:16 -0500

    Created a folder to hold the commands seperately, to keep the base file cleaner

--- discordbot.py
+++ discordbot.py
@@ -10,7 +10,7 @@ import nacl
 import os
 from dotenv import load_dotenv
 import asyncio
-
+from Commands.help import CustomHelpCommand
 load_dotenv()
 
 
@@ -94,33 +94,7 @@ class SongSession:
         next_source = self.queue.pop(0)
         self.play(next_source, {}, vc)  # Play the next song
         
-class CustomHelpCommand(commands.HelpCommand):
-    def get_command_signature(self, command):
-        return f"!{command.qualified_name} {command.signature}"
-
-    async def send_bot_help(self, mapping):
-        embed = self.context.bot.embed_template()
-        embed.description = "Type `!help command` for more info on a command."
-
-        for cog, commands in mapping.items():
-            if cog:
-                cog_name = cog.qualified_name
-                cog_commands = [self.get_command_signature(c) for c in commands]
-                embed.add_field( value="\n".join(cog_commands), inline=False)
-            else:
-                other_commands = [self.get_command_signature(c) for c in commands]
-                embed.add_field(value="\n".join(other_commands), inline=False)
-
-        embed.set_footer(text="You can also type !help category for more info on a category.")
-        await self.get_destination().send(embed=embed)
-
-        
-    async def send_command_help(self, command):
-        embed = self.context.bot.embed_template()
-        embed.title = f"Command Help: {command.qualified_name}"
-        embed.description = command.help or "No help available."
-
-        await self.get_destination().send(embed=embed)       
+     
 
 class Bot(commands.Cog):
     def __init__(self, bot):
