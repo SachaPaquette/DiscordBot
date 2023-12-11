71d03c6a317bfd7adc6690a2e286fe8ba0e91de7
Sacha <le1829079@crc-lennox.qc.ca>
2023-12-11 17:35:58 -0500

    Added the youtube download file for fetching song url

---
+++ Commands/ytdl.py
@@ -0,0 +1,22 @@
+import discord
+from yt_dlp import YoutubeDL
+from dotenv import load_dotenv
+import asyncio
+
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+        self.data = data
+        self.title = data.get("title")
+        self.url = ""
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        YDL_OPTIONS = {"format": "bestaudio", "noplaylist": "True"}
+        with YoutubeDL(YDL_OPTIONS) as ydl:
+            info = ydl.extract_info(url, download=False)
+        URL = info["url"] 
+        print(URL)
+        return URL
--- discordbot.py
+++ discordbot.py
@@ -11,26 +11,10 @@ import os
 from dotenv import load_dotenv
 import asyncio
 from Commands.help import CustomHelpCommand
+from Commands.ytdl import YTDLSource
 load_dotenv()
 
 
-
-class YTDLSource(discord.PCMVolumeTransformer):
-    def __init__(self, source, *, data, volume=0.5):
-        super().__init__(source, volume)
-        self.data = data
-        self.title = data.get("title")
-        self.url = ""
-
-    @classmethod
-    async def from_url(cls, url, *, loop=None, stream=False):
-        loop = loop or asyncio.get_event_loop()
-        YDL_OPTIONS = {"format": "bestaudio", "noplaylist": "True"}
-        with YoutubeDL(YDL_OPTIONS) as ydl:
-            info = ydl.extract_info(url, download=False)
-        URL = info["url"] 
-        print(URL)
-        return URL
         
 
 
