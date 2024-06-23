1d693a7d2bcd1451e1a68162f0d819a9bb96177b
sach <sachapaquette12@gmail.com>
2024-06-22 23:32:04 -0400

    Remade the algorithm for the work command

--- Commands/work.py
+++ Commands/work.py
@@ -1,3 +1,4 @@
+# Simulate working and gives a random amount of money
 import time, random
 from Config.logging import setup_logging
 from Config.config import conf
@@ -7,27 +8,19 @@ logger = setup_logging("work.py", conf.LOGS_PATH)
 class Work():
     def __init__(self):
         self.database = Database.getInstance()
-        
     async def work_command(self, interactions):
         try:
-            # Simulate working and gives a random amount of money
-            user_id = interactions.user.id
-            user = self.database.get_user(interactions.guild.id,user_id)
+            # Get the user's data
+            user = self.database.get_user(interactions.guild.id, interactions.user.id)
             # Check if the user has worked in the last 10 minutes
-            if "last_work" in user:
-                if user["last_work"] + 600 > time.time():
-                    await interactions.response.send_message("You can only work every 10 minutes.")
-                    return
-            
-            # Calculate a random amount of money earned
+            if user.get("last_work", 0) + 600 > time.time():
+                await interactions.response.send_message("You can only work every 10 minutes.")
+                return
             money_earned = random.randint(1, 1000)
-            
             # Update the user's balance
-            user["balance"] += money_earned
-            self.database.update_user_balance(interactions.guild.id, user_id, user["balance"],0, True)
-            
+            self.database.update_user_balance(interactions.guild.id, interactions.user.id, user["balance"] + money_earned, 0, True)
             # Send a message to the user
-            await interactions.response.send_message(f"Congratulations! You earned ${money_earned}, you now have {user['balance']:.2f} dollars.")
+            await interactions.response.send_message(f"Congratulations! You earned ${money_earned}, you now have {(user['balance'] + money_earned):.2f} dollars.")
         except Exception as e:
             logger.error(f"Error in the work function in gambling.py: {e}")
             return
