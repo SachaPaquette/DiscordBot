e797296cf6c2bd0703f460324a75290d234d165f
sach <sachapaquette12@gmail.com>
2024-06-28 11:36:57 -0400

    Made the profanity class to be a singleton

--- Commands/profanity.py
+++ Commands/profanity.py
@@ -7,20 +7,28 @@ from Config.config import conf
 logger = setup_logging("profanity.py", conf.LOGS_PATH)
 
 class Profanity():
+    _instance = None
+
+    def __new__(cls, bot):
+        if not cls._instance:
+            cls._instance = super(Profanity, cls).__new__(cls)
+            cls._instance._initialized = False
+        return cls._instance
+
     def __init__(self, bot):
+        # Initialize the class only once
+        if self._initialized:
+            return
         self.bot = bot
         self.profanity_filter = profane()
         self.utility = Utility()
-        
+        self._initialized = True
+
     async def on_message_command(self, message):
         try:
             # Check that it is not a message from the bot or an emoji reaction (:emoji:)
-            if message.author.bot or self.utility.is_emoji(message.content):
-                return     
-            # Check if the message contains profanity
-            if self.profanity_filter.contains_profanity(message.content):
-                # Put a reaction on the user's message to warn them
-                await message.add_reaction("🚫")
+            if not message.author.bot and not self.utility.is_emoji(message.content) and self.profanity_filter.contains_profanity(message.content):
+                await message.add_reaction("🚫")  
         except Exception as e:
             logger.error(f"Error while handling message: {e}")
             return
