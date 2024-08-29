8134e8292a1ddd3b7b6e6143033940cae4522bbe
sach <sachapaquette12@gmail.com>
2024-08-28 23:17:27 -0400

    Refactored work class to be cleaner and have a smaller work_command function

--- Commands/work_command.py
+++ Commands/work_command.py
@@ -9,18 +9,7 @@ logger = setup_logging("work.py", conf.LOGS_PATH)
 
 class Jobs():
     def __init__(self):
-        # Create job based on the user's level
-        # Between level 1 and 10, the user can work as a cashier
-        # Between level 11 and 20, the user can work as a waiter
-        # Between level 21 and 30, the user can work as a bartender
-        # Between level 31 and 40, the user can work as a chef
-        # Between level 41 and 50, the user can work as a manager
-        # Between level 51 and 60, the user can work as a CEO
-        # Between level 61 and 70, the user can work as a president
-        # Between level 71 and 80, the user can work as a king
-        # Between level 81 and 90, the user can work as a god
-        # Between level 91 and 99, the user can work as a creator
-        
+        # Create a job based on the user's level    
         self.jobs = {
             1: {"title": "cashier", "icon": "🛒", "min_earnings": 1, "max_earnings": 100, "description": "You work as a cashier in a small store. You earn between $1 and $100."},
             11: {"title": "waiter", "icon": "🍽️", "min_earnings": 101, "max_earnings": 200, "description": "You work as a waiter in a restaurant. You earn between $101 and $200."},
@@ -36,35 +25,39 @@ class Jobs():
         
         
     def get_job(self, level):
-        # Get the job based on the user's level
-        low = 0
-        high = len(self.jobs) - 1
-        while low <= high:
-            mid = (low + high) // 2
-            if level < list(self.jobs.keys())[mid]:
-                high = mid - 1
-            elif level >= list(self.jobs.keys())[mid]:
-                low = mid + 1
-        return self.jobs[list(self.jobs.keys())[high]]
+        # Get the job corresponding to the user's level
+        for lvl in sorted(self.jobs.keys(), reverse=True):
+            if level >= lvl:
+                return self.jobs[lvl]
+        return None
 
 class Work():
     def __init__(self):
         self.database = Database.getInstance()
         self.jobs = Jobs()
         self.embedMessage = EmbedMessage()
+        
+    def get_amount_earned(self, job):
+        return random.randint(job["min_earnings"], job["max_earnings"])
+        
+    def has_worked(self, user):
+        return user.get("last_work", 0) + 600 > time.time()
+        
     async def work_command(self, interactions):
         try:
             # Get the user's data
             user = self.database.get_user(interactions)
+            
             # Check if the user has worked in the last 10 minutes
-            if user.get("last_work", 0) + 600 > time.time():
+            if self.has_worked(user):
                 await interactions.response.send_message("You can only work every 10 minutes.")
-                return              
+                return        
+                  
             # Get the user's job
             job = self.jobs.get_job(user.get("level", 1))
             
             # Get a random amount of money between the min and max earnings of the job
-            amount_earned = random.randint(job["min_earnings"], job["max_earnings"])
+            amount_earned = self.get_amount_earned(job)
 
             # Update the user's balance
             self.database.update_user_balance(interactions.guild.id, interactions.user.id, user["balance"] + amount_earned, 0, True)
