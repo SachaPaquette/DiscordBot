def5a8e70892444b97554457d56454a98e8a1376
sach <sachapaquette12@gmail.com>
2024-08-30 20:42:23 -0400

    Refactored the coin flip command

--- Commands/coinflip_command.py
+++ Commands/coinflip_command.py
@@ -10,8 +10,8 @@ from enum import Enum
 logger = setup_logging("roll.py", conf.LOGS_PATH)
 
 class CoinChoices(Enum):
-    HEADS = "heads"
-    TAILS = "tails"
+    HEADS = "🪙"
+    TAILS = "🃏"
 
 class CoinFlip():
     def __init__(self):
@@ -20,97 +20,102 @@ class CoinFlip():
         self.embedMessage = EmbedMessage()
     async def coinflip_command(self, interactions, bet: float, opponent: discord.Member):
         try:
-            # Check if the user is betting against themselves
-            if opponent.id == interactions.user.id:
-                await interactions.response.send_message(f'{interactions.user.mention}, you can\'t bet against yourself.')
+            await self.check_bet_validity(interactions, bet, opponent)
+            command_user, opposing_user = await self.get_users(interactions, opponent)
+            message = await self.send_reaction_message(interactions, opponent)
+            reactions = await self.get_reactions(interactions, message)
+            if reactions[0][0].emoji == reactions[1][0].emoji:
+                await interactions.edit_original_response(content='Both users cannot choose the same option. The game is canceled.')
                 return
+            winner, winnings = await self.determine_winner(interactions, reactions, bet)
+            await self.update_balances(interactions, command_user, opposing_user, winner, winnings, bet)
+            await self.send_result_message(interactions, bet, winner, winnings, CoinChoices(reactions[0][0].emoji), reactions[0][0].emoji, command_user, opposing_user)
+        except asyncio.TimeoutError:
+            await interactions.edit_original_response(content='The game timed out. Both users need to react within 60 seconds.')
+
+        except Exception as e:
+            logger.error(f"Error in the coinflip function: {e}")
+            await interactions.edit_original_response(content='An error occurred. Please try again.')
+
+    async def check_bet_validity(self, interactions, bet, opponent):
+        if opponent.id == interactions.user.id:
+            await interactions.response.send_message(f'{interactions.user.mention}, you can\'t bet against yourself.')
+            raise Exception
+        if opponent.bot:
+            await interactions.response.send_message(f'{interactions.user.mention}, you can\'t bet against a bot.')
+            raise Exception
+        if bet <= 0:
+            await interactions.response.send_message(f'{interactions.user.mention}, you must bet a positive amount.')
+            raise Exception
+
+    async def get_users(self, interactions, opponent):
+        command_user = self.database.get_user(interactions)
+        opposing_user = self.database.get_user(interactions, opponent.id)
+        return command_user, opposing_user
+
+    async def send_reaction_message(self, interactions, opponent):
+        try:
+            await interactions.response.send_message(
+                f'{interactions.user.mention} and {opponent.mention}, choose heads (🪙) or tails (🃏).')
             
-            
-            # Check if the opponent is a bot
-            if opponent.bot:
-                await interactions.response.send_message(f'{interactions.user.mention}, you can\'t bet against a bot.')
-                return
-            
-            # Check if the bet is positive
-            if bet <= 0:
-                await interactions.response.send_message(f'{interactions.user.mention}, you must bet a positive amount.')
-                return
-
-            command_user = self.database.get_user(interactions)
-            opposing_user = self.database.get_user(interactions, opponent.id)
-
-            # Check if both users have enough money
-            if not self.utility.has_sufficient_balance(command_user, bet):
-                await interactions.response.send_message(f'{interactions.user.mention}, you don\'t have enough money to bet that amount.')
-                return
-
-            if not self.utility.has_sufficient_balance(opposing_user, bet):
-                await interactions.response.send_message(f'{opponent.mention} doesn\'t have enough money to bet that amount.')
-                return
-
-            # Send a message with an emoji to react to
-            self.message = await interactions.response.send_message(
-                f'{interactions.user.mention} and {opponent.mention}, choose heads (🪙) or tails (🃏).', ephemeral=False)
-
             message = await interactions.original_response()
-
-            # Add reactions to the message
             await message.add_reaction("🪙")
             await message.add_reaction("🃏")
-
-            # Check function to ensure both users react
-            def check(reaction, user):
-                return user in [interactions.user, opponent] and str(reaction.emoji) in ["🪙", "🃏"]
-
-            # Wait for reactions from both users
-            command_user_reaction = await interactions.client.wait_for('reaction_add', timeout=60.0, check=check)
-            opposing_user_reaction = await interactions.client.wait_for('reaction_add', timeout=60.0, check=check)
-
-            if command_user_reaction[0].emoji == opposing_user_reaction[0].emoji:
-                await message.edit(content='Both users cannot choose the same option. The game is canceled.')
-                return
-
-
-            # Deduct the bet from the user's balance
+            return message
+        except Exception as e:
+            logger.error(f"Error sending the reaction message: {e}")
+            return
+        
+    async def get_reactions(self, interactions, message):
+        try:
+            def command_user_check(reaction, user):
+                return user == interactions.user and str(reaction.emoji) in ["🪙", "🃏"]
+            
+            def opposing_user_check(reaction, user):
+                return user == message.mentions[0] and str(reaction.emoji) in ["🪙", "🃏"]
+            
+            command_user_reaction = await interactions.client.wait_for('reaction_add', timeout=60.0, check=command_user_check)
+            opposing_user_reaction = await interactions.client.wait_for('reaction_add', timeout=60.0, check=opposing_user_check)
+            return command_user_reaction, opposing_user_reaction
+        except asyncio.TimeoutError:
+            raise asyncio.TimeoutError
+        except Exception as e:
+            logger.error(f"Error getting reactions: {e}")
+            return
+        
+    async def determine_winner(self, interactions, reactions, bet):
+        try:
+            coin = random.choice([CoinChoices.HEADS, CoinChoices.TAILS])
+            result_emoji = "🪙" if coin == CoinChoices.HEADS else "🃏"
+            winner = interactions.user if reactions[0][0].emoji == result_emoji else interactions.message.mentions[0]
+            winnings = bet * 2
+            return winner, winnings
+        except Exception as e:
+            logger.error(f"Error determining the winner: {e}")
+            return None, None  # Return None explicitly
+    async def update_balances(self, interactions, command_user, opposing_user, winner, winnings, bet):
+        try:
+            
             command_user["balance"] -= bet
             opposing_user["balance"] -= bet
-
-            # Flip a coin
-            coin = random.choice([CoinChoices.HEADS, CoinChoices.TAILS])
-            
-            # Determine the result emoji
-            result_emoji = "🪙" if coin == CoinChoices.HEADS else "🃏"
-
-            # Determine the winner based on reactions
-            winner = interactions.user if result_emoji in [reaction.emoji for reaction in message.reactions if reaction.count > 1 and reaction.users().first() == interactions.user] else opponent
-
-            # Calculate the winnings
-            winnings = bet * 2
-
-            # Add the winnings to the winner's balance
             if winner == interactions.user:
                 command_user["balance"] += winnings
             else:
                 opposing_user["balance"] += winnings
-
-            # Update the balances in the database
             self.database.update_user_balance(interactions.guild.id, interactions.user.id, command_user["balance"])
-            self.database.update_user_balance(interactions.guild.id, opponent.id, opposing_user["balance"])
-
-            # Send a message with the result
-            #await interactions.followup.send(f'{interactions.user.mention} bet {bet} dollars. The coin landed on {coin.value}. {winner.display_name} won {winnings} dollars.')
-
-            embed = self.embedMessage.create_coinflip_embed_message(interactions, bet, coin.value, result_emoji, winner.display_name, winnings, command_user["balance"], opposing_user["balance"])
-
-            await message.edit(embed=embed)
-            
-        except asyncio.TimeoutError:
-            await message.edit(content='The game timed out. Both users need to react within 60 seconds.')
-
+            self.database.update_user_balance(interactions.guild.id, interactions.message.mentions[0].id, opposing_user["balance"])
         except Exception as e:
-            print(f"Error in the coinflip function: {e}")
-            await message.edit(content='An error occurred. Please try again.')
-
+            logger.error(f"Error updating balances: {e}")
+            return
+        
+    async def send_result_message(self, interactions, bet, winner, winnings, coin, result_emoji, command_user, opposing_user):
+        try:
+            
+            embed = self.embedMessage.create_coinflip_embed_message(interactions, bet, coin.value, result_emoji, winner.display_name, winnings, command_user["balance"], opposing_user["balance"])
+            await interactions.edit_original_response(embed=embed)
+        except Exception as e:
+            logger.error(f"Error in the coinflip function in gambling.py: {e}")
+            return
         
        
         
